ac6dbfe4efe592fff09b67c039a43755ec884dcd
Júlio Santos <julio@lifeonmars.pt>
2017-02-18 19:44:49 +0700

    Add screenshot upload endpoint

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,5 +1,7 @@
 class Screenshot < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+  validates :image, presence: true
+
   belongs_to :screenshootable, polymorphic: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :modules, only: [:index, :show]
+    resources :screenshots, only: [:create]
   end
 
   # DEVISE

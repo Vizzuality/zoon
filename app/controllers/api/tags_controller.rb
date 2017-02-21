e41919aee64e1ff98fb3ab54aa0d8bd795604c89
Paulo Koch <paulo.koch@gmail.com>
2017-02-21 19:31:00 +0700

    Remove duplicate tags

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,7 +1,7 @@
 class Api::TagsController < ApplicationController
   def index
     render json: {
-      tags: Tag.where("name ILIKE ?", "#{params[:search]}%").pluck(:name),
+      tags: Tag.where("name ILIKE ?", "#{params[:search]}%").distinct.pluck(:name)
     }
   end
 end

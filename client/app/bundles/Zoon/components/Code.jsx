669a7f6e0cfce2b77660ea5c9b819dec0cf8ec58
Paulo Koch <paulo.koch@gmail.com>
2017-03-07 16:44:27 +0000

    Implement copy button

--- client/app/bundles/Zoon/components/Code.jsx
+++ client/app/bundles/Zoon/components/Code.jsx
@@ -1,8 +1,43 @@
 import React from "react"
+import copy from "copy-to-clipboard"
 
-export default ({children}) => (
-  <div className="code">
-    <div className="code__content">{children}</div>
-    <div className="code__copy"><i className="fa fa-clone" /></div>
-  </div>
-)
+class Code extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      showingTooltip: false,
+    }
+  }
+
+  onClick = () => {
+    this.timeoutId && clearTimeout(this.timeoutId)
+
+    copy(this.props.children)
+
+    this.setState({showingTooltip: true})
+    this.timeoutId = setTimeout(
+      () => {
+        this.setState({showingTooltip: false})
+        this.timeoutId = null
+      },
+      2000,
+    )
+  }
+
+  render () {
+    return <div className="code">
+      <div className="code__content">
+        {this.props.children}
+      </div>
+      <div className="code__copy tooltip-context">
+        <i className="fa fa-clone" onClick={this.onClick} />
+        <div className={`tooltip ${this.state.showingTooltip && "on" || ""}`}>
+          Copied!
+        </div>
+      </div>
+    </div>
+  }
+}
+
+export default Code

c7946205b484fefda676357cfe4dc461d2c5c089
Simao Belchior <simaobelchior@gmail.com>
2017-04-02 17:55:44 +0100

    Homepage text chagnes suggested by the client

--- client/app/bundles/Zoon/components/Home.jsx
+++ client/app/bundles/Zoon/components/Home.jsx
@@ -39,7 +39,7 @@ class Home extends React.Component {
         <F.Row>
           <F.Column small={12} large={8}>
             <h1>ZOON</h1>
-            <p>Modelling isn’t always easy, but it could be easier. ZOON reduces the time and effort it takes to find data, create species distribution models, and share them with the world.</p>
+            <p>ZOON reduces the time and effort it takes to find data, create species distribution models, and share that work. A step towards modifiable and reproducible modelling.</p>
           </F.Column>
         </F.Row>
 
@@ -47,9 +47,9 @@ class Home extends React.Component {
 
         <F.Row>
           <F.Column small={12} large={8}>
-            <h3>CREATE</h3>
+            <h3>EXPLORE MODULES</h3>
             <p>
-              ZOON is an R package for Species Distribution Modelling* that allows you to develop your analysis as a reproducible workflow composed of 5 types of module (species <Link to="/modules?searchFamily=ocurrence">occurrence</Link> data, <Link to="/modules?searchFamily=covariate">covariate</Link> data, <Link to="/modules?searchFamily=process">pre-processing</Link>, the <Link to="/modules?searchFamily=model">model</Link> and the <Link to="/modules?searchFamily=output">output</Link>).
+              ZOON is an R package for Species Distribution Modelling* that allows you to develop your analysis as a reproducible workflow composed of 5 types of module (species <Link to="/modules?searchFamily=occurrence">occurrence</Link> data, <Link to="/modules?searchFamily=covariate">covariate</Link> data, <Link to="/modules?searchFamily=process">pre-processing</Link>, the <Link to="/modules?searchFamily=model">model</Link> and the <Link to="/modules?searchFamily=output">output</Link>).
             </p>
             <ul className="moduleTypes">
               {["occurrence", "covariate", "process", "model", "output"].map((type) => (
@@ -82,23 +82,34 @@ class Home extends React.Component {
 
             <hr />
 
-            <h3>PUBLISH</h3>
+            <h3>CREATE WORKFLOWS</h3>
             <p>
-              The current method of creating and publishing species distribution models doesn’t lend itself to conversation, modification or replication. Despite some amazing pieces of work being available to read we cannot always access the data and code.
+              Traditional publishing and modelling practices don’t always help us to access, understand, modify or replicate Species Distribution Modelling studies. Despite the quality of the scientific work, without access to data and code we may not be able to do anything more than read the paper.
             </p>
             <p>
               Using the workflow publisher you can increase the accessibility, usability and quality of your work, or find existing work to inspire your latest research.
             </p>
 
             <Link to="/workflows/new" className="button">
-              Try the Workflow Publisher
+              Try the Workflow Creator
+            </Link>
+
+            <hr />
+
+            <h3>SHARE</h3>
+            <p>
+              You can upload your own work to the ZOON repository by submitting your modules and workflows here. Create tags, upload screen shots and get the most out of your work by enabling others.
+            </p>
+
+            <Link to="/workflows/new" className="button">
+              Share your work
             </Link>
 
             <hr />
 
             <h3>DOWNLOAD NOW!</h3>
             <p>
-              Whether you’re just getting started in Species Distribution Modelling*, or a very experienced scientist or scientific software engineer, ZOON offers a better way to develop our science. There are lots of modelling techniques and software available**. ZOON can use all of the resources available in R.
+              Whether you’re just getting started in Species Distribution Modelling*, or a very experienced scientist or scientific software engineer, ZOON offers a better way to develop our science. There are lots of modelling techniques and software available**. With ZOON, you can use all of the resources available in R.
             </p>
             <p>
               ZOON is on CRAN: <a href="https://cran.r-project.org/web/packages/zoon/index.html">https://cran.r-project.org/web/packages/zoon/index.html</a>
@@ -117,8 +128,26 @@ class Home extends React.Component {
             <Code>library(zoon)</Code>
 
             <p>
-              See the <a href="#">tutorials</a> for more details.
+              See the tutorials for more details.
             </p>
+
+            <ul>
+              <li>
+                <a href="http://rawgit.com/zoonproject/zoon/master/inst/doc/basic-zoon-usage.html">
+                  Usage
+                </a>
+              </li>
+              <li>
+                <a href="http://rawgit.com/zoonproject/zoon/master/inst/doc/Building_a_module.html">
+                  Building a module
+                </a>
+              </li>
+              <li>
+                <a href="http://rawgit.com/zoonproject/zoon/master/inst/doc/interactive_zoon_usage.html ">
+                  Interactive ZOON usage
+                </a>
+              </li>
+            </ul>
 
             <hr />
 

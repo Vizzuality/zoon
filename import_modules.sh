c4e3f2e0064fba94d89d22937a1a4acbd61b0d5d
Paulo Köch <pkoch@lifeonmars.pt>
2017-06-21 15:56:15 +0100

    Make module order deterministic

--- import_modules.sh
+++ import_modules.sh
@@ -5,5 +5,5 @@ TARGET="modules-master"
 rm -rf $TARGET master.zip
 wget --no-verbose https://github.com/zoonproject/modules/archive/master.zip
 unzip -q master
-find $TARGET/R -name "*.R" | xargs cat | Rscript ~/R/module2json.R | bin/rake import:modules[-]
+find $TARGET/R -name "*.R" | sort | xargs cat | Rscript ~/R/module2json.R | bin/rake import:modules[-]
 rm -rf $TARGET master.zip
